30ead288bac44a3c77f8fa2158df4760e296f600
Reid Mewborne <9111792+8bitreid@users.noreply.github.com>
2022-03-29 22:00:49 -0400

    update zio-json to support zio version

--- README.md
+++ README.md
@@ -2,12 +2,5 @@
 
 Using ZIO and ZIO Json.
 
-## ERROR
-```
-[info] running example.Hello 
-timestamp=2022-03-30T01:43:02.355362Z level=ERROR thread=#zio-fiber-0 message="" cause="Exception in thread "zio-fiber-2" java.lang.NoClassDefFoundError: java.lang.NoClassDefFoundError: zio/ZManaged
-        at example.Hello.myAppLogic(Hello.scala:14)"
-```
-
 #### TODO
 - [ ] add zio-http
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,6 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
   lazy val zio = "dev.zio" %% "zio" % zioVersion
   lazy val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
-  lazy val zioJson = "dev.zio" %% "zio-json" % "0.3.0-RC3"
+  lazy val zioJson = "dev.zio" %% "zio-json" % "0.3.0-RC5"
   lazy val zioTest = "dev.zio" %% "zio-test" % zioVersion
 }
