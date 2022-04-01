308797c77754a5d862ede1bc7f7638ff70c9ea26
Reid Mewborne <9111792+8bitreid@users.noreply.github.com>
2022-04-01 15:49:04 -0400

    fix deps and add test

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,12 @@ import sbt._
 
 object Dependencies {
   val zioVersion = "2.0.0-RC3"
+  val ZHTTPVersion = "1.0.0.0-RC24"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
   lazy val zio = "dev.zio" %% "zio" % zioVersion
   lazy val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
   lazy val zioJson = "dev.zio" %% "zio-json" % "0.3.0-RC5"
   lazy val zioTest = "dev.zio" %% "zio-test" % zioVersion
+  lazy val zhttp = "io.d11" %% "zhttp" % ZHTTPVersion
+  lazy val zhttpTest = "io.d11" %% "zhttp-test" % ZHTTPVersion
 }
