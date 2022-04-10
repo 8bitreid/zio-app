353b18cc3cd7e28292353798dedfa94b18ed6c3e
Reid Mewborne <9111792+8bitreid@users.noreply.github.com>
2022-04-10 13:53:06 -0400

    output zio test in sbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,7 @@ object Dependencies {
   lazy val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
   lazy val zioJson = "dev.zio" %% "zio-json" % "0.3.0-RC5"
   lazy val zioTest = "dev.zio" %% "zio-test" % zioVersion
+  lazy val zioTestSbt = "dev.zio" %% "zio-test-sbt" % zioVersion
   lazy val zhttp = "io.d11" %% "zhttp" % ZHTTPVersion
   lazy val zhttpTest = "io.d11" %% "zhttp-test" % ZHTTPVersion
 }
