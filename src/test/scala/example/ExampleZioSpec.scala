ea98c7ebc6b2024f95100862ef29522d569f6724
Reid Mewborne <9111792+8bitreid@users.noreply.github.com>
2022-04-10 13:47:23 -0400

    uses coursier/cache-action@v6

--- src/test/scala/example/ExampleZioSpec.scala
+++ src/test/scala/example/ExampleZioSpec.scala
@@ -7,7 +7,7 @@ object ExampleZioSpec extends DefaultRunnableSpec {
   def spec: Spec[Any, TestFailure[Nothing], TestSuccess] =
     suite("ABC payment provider tests") {
       test("Your test")(
-        assert("Your value")(Assertion.isEmptyString)
+        assert("Your value")(Assertion.isNonEmptyString)
       )
     }
 }
